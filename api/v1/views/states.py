afb7bd2c3d2b67a7bed86d1710b24e3eac6f855d
Olyad Temesgen Amsalu <105017653+Olyadtemesgen@users.noreply.github.com>
2023-03-01 23:50:18 +0300

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
-
+# Amazingly speaking this does mean that I know dhango
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/state/get_state.yml', methods=['GET'])
 def get_states():
